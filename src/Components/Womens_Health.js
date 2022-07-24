cbddf8b99fdda35a4bf70d7cb588ad79ad7c53b6
Vivian Liu <41202975+vzliu@users.noreply.github.com>
2022-07-24 19:48:45 -0400

    women's health set up

--- src/Components/Womens_Health.js
+++ src/Components/Womens_Health.js
@@ -1,13 +1,32 @@
 import './Womens_Health.css';
 import image from './Images/Background_Home.jpg';
+import image1 from './Images/pic_of_health.png';
 
 function Womens_Health() {
     return (  
+        //put yo code inside div, dont need to reload page
         <div className='print'>
-            <h1>Womens Health goes here</h1>
-            <p className='abc'>Women should care for themselves</p>
-            <img className='image' src={image}></img>
+            <h1 className = 'women_health_center'>Women's Health</h1>
+            <h3 className='abc'><b>Here are some resources for women for various situations ranging from birth control,
+                abortions, pregnancy, and more. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</b></h3>
+            {/*<img className='image' src={image}></img>*/}
+
+            <h2 className = 'abcd'> Birth Control</h2>
+
+            <h2 className = 'abcd'> Sexually Transmitted Diseases</h2>
+            
+            <h2 className = 'abcd'> Abortions </h2>
+
+            <h2 className = 'abcd'> Caregiving</h2>
+
+            <h2 className = 'abcd'> Pregnancy</h2>
+
+            <h2 className = 'abcd'> Mental Health Support</h2>
+            
+            <img className='image' src={image1}></img>
+
         </div>
+        //can only return one div
     );
 }
 
