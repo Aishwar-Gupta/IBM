3691b9ba07c2cb1169034004e561f7e01e915b5d
Vivian Liu <41202975+vzliu@users.noreply.github.com>
2022-07-25 19:06:28 -0400

    Update Womens_Health.js

--- src/Components/Womens_Health.js
+++ src/Components/Womens_Health.js
@@ -7,26 +7,105 @@ function Womens_Health() {
         //put yo code inside div, dont need to reload page
         <div className='print'>
             <h1 className = 'women_health_center'>Women's Health</h1>
-            <h3 className='abc'><b>Here are some resources for women for various situations ranging from birth control,
-                abortions, pregnancy, and more. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</b></h3>
+            <h3 className='abc'><b>Below we explain outline some important things to know about regarding women's health. We also outline some resources for women for various situations ranging from birth control,
+                abortions, pregnancy, and more.</b></h3>
             {/*<img className='image' src={image}></img>*/}
 
             <h2 className = 'abcd'> Birth Control</h2>
+            <p>
+            <ul><li>Sterilization</li></ul> 
+            
+
+            Tubal ligation/Vasectomy 
+            One time procedure that is permanent
+            Long Acting Reversible Contraceptives
+            IUD  
+            last up to years depending
+            Short Acting Hormonal Methods
+            birth control pill 
+            must take daily
+            Barrier Methods
+            Male/Female Condoms
+            must use every time you have sex
+            </p>
 
             <h2 className = 'abcd'> Sexually Transmitted Diseases</h2>
-            
+            <p>Bacterial Vaginosis is a condition that happens when there is too much of certain bacteria in the vagina that it alters the main bacteria in the vagina. This is the most common vaginal condition in women ages 15-44. It is caused by unsafe sexual activity and is a result of an imbalance of “good” and “harmful” bacteria in a vagina
+            </p>
+            <p>Chlamydia is an infection that can affect both men and women and damage a woman's reproductive system and can cause potentially fatal ectopic pregnancy. It can be caused by unsafe sexual activity and has unnoticable symmptoms that include abnormal genatail discharge and burning sensation when peeing. 
+            </p>
+            <p>Gonorrhea can cause infection in the genitals, rectum, and throat and is very common among people around the ages 15 - 25 years and can cause a pregnant person to give their infection to their baby during childbirth. It is caused by unsafe sexual activity and often have no synoptoms beyond a buring senseation when peeing, increased discharge, and vaginal bleeding. 
+            </p>
+            <p>HPV is the most common STI and there are several different types of HPV that can cause various health problems.  It is caused by unsafe sexual activity and there are vaccines that can cure the sympotoms.
+            </p>
+            <p>For more information, you can refer to these websites:
+                <ul><li>https://www.cdc.gov/std/default.htm</li></ul>
+                <ul><li>https://www.cdc.gov/std/healthcomm/fact_sheets.htm</li></ul>
+
+            </p>
+           
+
             <h2 className = 'abcd'> Abortions </h2>
+            <p>Abortion Procedure:	"After the cervix is stretched open, the doctor inserts a hard plastic tube into the uterus, then connects this tube to a suction machine. The suction pulls the fetus' body apart and out of the uterus. The doctor may also use a loop-shaped knife called a curette to scrape the fetus and fetal parts out of the uterus."</p>
+            <p><ul><li>https://foundationsoflife.org/facts-about-abortion/abortion-procedure-facts/#:~:text=After%20the%20cervix%20is%20stretched,parts%20out%20of%20the%20uterus.
+</li></ul></p>
 
+            <p>Reasons why a woman would get an abortion:	A women could want to get an abortion having a baby would drastically change their life by interfereing with their education, job,employpemt/ career. They might be able to afford a baby or child care, don't want to be a single parent or are a victim or rape or incest.
+</p>
+<p><ul><li>https://www.guttmacher.org/journals/psrh/2005/reasons-us-women-have-abortions-quantitative-and-qualitative-perspectives
+</li></ul></p>
+            <p>Risks of an abortion:	 Abortions are generally very safe and most women will not experience any problems. Having an abortion does not increase the risk of breast cancer or menal health issues nor will affect your chances of becoming pregnant and ahving a normal pregancies in the future. Possible complications could include needing another procedure to remove parts of the pregnancy that have stayed in the womb, heavy bleeding, damage to the womb, or sepsis. 
+</p>
+
+<p><ul><li>https://www.nhs.uk/conditions/abortion/risks/
+</li></ul></p>
+<p>Types of abortions avaliable: You could have an in clinic abortion where the doctor or nurse will use medical instruments and gentle suction to remove the pregenancy from your uterus. This procedure is very effective and will usually take about 10 minutes. An medication abortion will include you taking a pill that will end your pregnancy and make your uterus expel the pregnancy tissue and is also very safe and effective.
+</p>
+<p><ul><li>https://www.plannedparenthood.org/learn/abortion/considering-abortion/what-facts-about-abortion-do-i-need-know
+</li></ul></p>
+<p>https://www.fpawomenshealth.com/abortion/abortion-out-state-patients:	As long as you can travel to California, we are proud to offer anyone "in-person" care no matter where they call home.  In fact, we strive to be accessible to those patients who may find themselves in a state without options for the care they need.
+</p>
+<p><ul><li>https://providecare.org/financial-resources-abortion/#:~:text=Contact%20the%20National%20Abortion%20Federation%20Hotline%20at%201%2D800%2D772,women%20pay%20for%20abortion%20services.
+</li></ul></p>
+<p><ul><li>https://www.plannedparenthood.org/learn/pregnancy/pregnancy-options
+</li></ul></p>
+<p><ul><li>https://www.reddit.com/r/Feminism/comments/phrcrn/this_is_a_comprehensive_list_of_resources_for/
+</li></ul></p>
             <h2 className = 'abcd'> Caregiving</h2>
-
+<p>https://www.fda.gov/consumers/womens-health-topics/caring-others-resources-help-you
+</p>
+            
             <h2 className = 'abcd'> Pregnancy</h2>
+<p>What is pregnancy?: "Pregnancy occurs when a sperm fertilizes an egg after it’s released from the ovary during ovulation. The fertilized egg then travels down into the uterus, where implantation occurs. A successful implantation results in pregnancy."
+</p>
+<p>On average, a full term pregnancy lasts 40 weeks. Regularly visit your health care provider and proper prenatal care will help make sure that you and your baby are healthy. 
+</p>
+<p>Symptoms of pregnancy: When you are pregnant your hormone levels will change and miss you period. You'll have headaches, spotting, weight gain, hypertension, , heartburn, constipation, cramps, back pain, anemia, depression, insomnia, breast changes, acne, vomiting, hip pain, and stress. 
+</p>
+<p>Eating healthy foods like protein, complex carbohydrates, vegetables, fruits, grains, healthy fats,  and drinking plenty of water. A pregnancy diet should include within 340 to 450 additional calories per day alongside a typical healthy diet. Gentle exercise is also recommended to keep you fit and ready for labor. 
+</p>
+<p>Pregnant women require a larger amount of vitamins and minerals that include folic acid, iron, calcium, vitamin D, choline, omega-3 fatty acids, B vitamins, and vitamin C. You can get these through supplements or over the counter medication advised by your doctor. 
+</p>
+<p>Some pregnancy complications include gestational diabetes, preeclampsia, preterm labor, and miscarraiage this can affect the both the baby and mother and can happen during pregnancy or labor.  Underlying conditions during pregnancy could include high blood pressure, diabetes, cardiovascular disease, cancer, kidney disease, epilepsy. This can lead to miscarriage, poor fetal growth, and birth defects if not treated and monitored. 
+</p>
+<p><ul><li>https://www.healthline.com/health/pregnancy#Pregnancy-Symptoms:-10-Early-Signs-That-You-May-Be-Pregnant
+</li></ul></p>
 
             <h2 className = 'abcd'> Mental Health Support</h2>
+<p>Postpartum depression is depression that occurs after having a baby. Feelings of postpartum depression are more intense and last longer than those of “baby blues,” a term used to describe the worry, sadness, and tiredness many women experience after having a baby. Symptoms of postpartum depression include crying often, feeling of anger, withdrawn from loved ones, feeling numb or disconnected, stress and doubting your ability to care for the baby. 
+</p>
+<p><ul><li>https://www.nimh.nih.gov/health/topics/women-and-mental-health
+</li></ul></p>
+<p>Crisis Hotlines</p>
+<p><ul><li>National Maternal Mental Health Hotline: https://mchb.hrsa.gov/national-maternal-mental-health-hotline</li></ul></p>
+<p><ul><li>SAMHSA's National Helpline: https://www.samhsa.gov/find-help/national-helpline</li></ul></p>
+<p><ul><li>APA Crisis Hotlines: https://www.apa.org/topics/crisis-hotlines </li></ul></p>
             
             <img className='image' src={image1}></img>
 
         </div>
         //can only return one div
+        //notes: start with static then make dynamic later, start small, chatbot can be built completely independtly, timing and team resources, build something that demos
     );
 }
 
