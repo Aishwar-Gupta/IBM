f555b5df280e64fb8973183d3181c0ebfbac32ba
jlum67 <90434758+jlum67@users.noreply.github.com>
2022-07-25 16:42:43 -0700

    random

--- src/App.js
+++ src/App.js
@@ -7,20 +7,34 @@ import Home from './Components/Home';
 import Food_Resources from './Components/Food_Resources';
 import Womens_Health from './Components/Womens_Health';
 
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
   return (
-    <div className="App">
-      
-      <Routes>
-        <Route exact path='/' element={<Home/>}></Route>
-        <Route exact path='/Food-Resources' element={<Food_Resources/>}></Route>
-        <Route exact path='/Womens-Health' element={<Womens_Health/>}></Route>
-      </Routes>
-      
-      <Navbar></Navbar>
-      <Footer></Footer>
 
-    </div>
+    //<ChakraProvider>
+
+      <div className="App">
+
+
+        <Routes>
+            <Route exact path='/' element={<Home/>}></Route>
+            <Route exact path='/Food-Resources' element={<Food_Resources/>}></Route>
+            <Route exact path='/Womens-Health' element={<Womens_Health/>}></Route>
+          </Routes>
+          
+          <Navbar></Navbar>
+          <Footer></Footer>
+          
+
+      </div>
+
+    //</ChakraProvider> 
+
+
+
+
   );
 }
 
